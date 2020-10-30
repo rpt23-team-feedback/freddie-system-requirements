2708db75782dc9dd44119a8d7775e9cb4327f340
redperezz <kaicruzz91@gmail.com>
2020-10-30 13:21:29 -0700

    PR fix

--- public/client/index.js
+++ public/client/index.js
@@ -2,4 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import System from './components/System.js';
 
-ReactDOM.render(<System />, document.getElementById('app'));
+ReactDOM.render(<System />, document.getElementById('system-requirements'));
